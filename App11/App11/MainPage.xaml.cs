65cf5bca216ca4c2760a1fafab079666dea44fa2
Vidya Shukla <vidya.091209@gmail.com>
2018-06-09 09:00:55 +0530

    Add project files.

---
+++ App11/App11/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App11
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+            var time = DateTime.Now;
+            var tuple = new Tuple<DateTime>(time);
+            try
+            {
+                
+               var t = Expression.Property(Expression.Constant(tuple), "Item1");
+                
+            }
+            catch (Exception ex)
+            {
+                msg.Text+= ex.Message;
+            }
+
+        }
+    }
+}
